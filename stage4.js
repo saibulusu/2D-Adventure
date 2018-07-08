4dc500d8a3492d7bcfd8597f67b78255b92b080d
Sai Bulusu <sai.bulusu@gmail.com>
2018-07-07 23:21:19 -0700

    Stage 4 Update - Framework for story

--- stage4.js
+++ stage4.js
@@ -4,179 +4,74 @@ var canvasContext;
 
 var edge = 40;
 
-var xPos = 0;
+var xPos = edge;
 var yPos = 362;
 
 var playerWidth;
 var playerHeight;
 
-var speed = 10;
+var playerSpeed = 5;
 
-var state = 1;
+var player = "kid";
+
+var fireX = -100;
+var fireY = -100;
+
+var fireWidth;
+var fireHeight;
+
+var boomerangX = -100;
+var boomerangY = -100;
+
+var boomerangWidth;
+var boomerangHeight;
+
+var swordWidth;
+var swordHeight;
+
+var boomerangStartX = -100;
+var boomerangStartY = -100;
+
+var boomerangSpeedX = 0;
+var boomerangSpeedY = 0;
+
+var kidWidth;
+var kidHeight;
+
+var warriorWidth;
+var warriorHeight;
+
+var wizardWidth;
+var wizardHeight;
+
+var builderWidth;
+var builderHeight;
+
+var state = 0;
 
 var kidHealth = 100;
 var warriorHealth = 100;
 var wizardHealth = 80;
 
+var builderHealth = 50;
+
 var gameover = false;
 
-var boomerangX = -100;
-var boomerangY = -100;
+var builderX = 1000;
+var builderY = 362;
 
-var swordX = -100;
-var swordY = -100;
+window.onload = function() {
+  canvas = document.getElementById('gameCanvas');
+  canvasContext = canvas.getContext('2d');
 
-var fireX = -100;
-var fireY = -100;
-
-window.onload = function() { // when the page loads
-	// define the canvas to the webpage
-	canvas = document.getElementById('gameCanvas');
-	canvasContext = canvas.getContext('2d');
-
-	window.addEventListener('keydown', keyInput, false);
+  // key events are handled
+  window.addEventListener('keydown', keyInput, false);
   window.addEventListener('mousedown', mousedown, false);
 
-	// run 30 frames per second
-	var framesPerSecond = 60;
-	setInterval(updateAll, 1000/framesPerSecond);
+  // run 30 frames per second
+  var framesPerSecond = 60;
+  setInterval(updateAll, 1000/framesPerSecond);
 }
-
-function keyInput(event) { // key events to control the direction of the snake
-	// alert(event.keyCode);
-
-	wizard = new Image();
-	wizard.src = 'images/wizard.png';
-
-	wizardWidth = wizard.naturalWidth;
-	wizardHeight = wizard.naturalHeight;
-
-	sworddown = new Image();
-	sworddown.src = 'images/sworddown.png';
-
-	swordleft = new Image();
-	swordleft.src = 'images/swordleft.png';
-
-	swordup = new Image();
-	swordup.src = 'images/swordup.png';
-
-	swordright = new Image();
-	swordright.src = 'images/swordright.png';
-
-	swordleftwidth = swordleft.naturalWidth;
-	swordleftheight = swordleft.naturalHeight;
-
-	swordupwidth = swordup.naturalWidth;
-	swordupheight = swordup.naturalHeight;
-
-  swordrightwidth = swordright.naturalWidth;
-  swordrightheight = swordright.naturalWidth;
-
-  sworddownwidth = sworddown.naturalWidth;
-  sworddownheight = sworddown.naturalHeight;
-
-  if (state == 2) {
-    // code holds the number to map to a direction, using arrow-keys
-  	var code = event.keyCode;
-
-  	// make sure that the movement does not cause the snake to move into itself
-  	if (code == 65) { // left
-		left = true;
-  	} else if (code == 87) { // up
-		up = true;
-  	} else if (code == 68) { // right
-		right = true;
-  	} else if (code == 83) { // down
-		down = true;
-  	} else if (code == 74) { // a
-		if (player == "kid") {
-  			if (boomerangX == -100) {
-  				boomerangX = xPos;
-  				boomerangY = yPos;
-  				boomerangStartX = xPos;
-  				boomerangStartY = yPos;
-  				boomerangSpeedX = -10;
-  				boomerangSpeedY = 0;
-  			}
-  		} else if (player == "warrior") {
-				canvasContext.drawImage(swordleft, xPos - 30, yPos + warriorHeight / 2);
-				if (overlap(xPos - 30, yPos + warriorHeight / 2, xPos - 30 + swordleftwidth, yPos + warriorHeight / 2 + swordleftheight,
-				wizardX, wizardY, wizardX + wizardWidth, wizardY + wizardHeight)) {
-					wizardHealth -= 1;
-				}
-			}
-  	} else if (code == 73) { // up
-			if (player == "kid") {
-  			if (boomerangX == -100) {
-  				boomerangX = xPos;
-  				boomerangY = yPos;
-  				boomerangStartX = xPos;
-  				boomerangStartY = yPos;
-  				boomerangSpeedX = 0;
-  				boomerangSpeedY = -10;
-  			}
-  		} else if (player == "warrior") {
-				canvasContext.drawImage(swordup, xPos, yPos - 40);
-				if (overlap(xPos, yPos - 40, xPos + swordupwidth, yPos - 40 + swordupheight,
-				wizardX, wizardY, wizardX + wizardWidth, wizardY + wizardHeight)) {
-					wizardHealth -= 1;
-				}
-			}
-  	} else if (code == 76) { // right
-			if (player == "kid") {
-  			if (boomerangX == -100) {
-  				boomerangX = xPos;
-  				boomerangY = yPos;
-  				boomerangStartX = xPos;
-  				boomerangStartY = yPos;
-  				boomerangSpeedX = 10;
-  				boomerangSpeedY = 0;
-  			}
-  		} else if (player == "warrior") {
-				canvasContext.drawImage(swordright, xPos + warriorWidth / 2, yPos + warriorHeight / 2);
-				if (overlap(xPos + warriorWidth / 2, yPos + warriorHeight / 2, xPos + warriorWidth / 2 + swordleftwidth, yPos + warriorHeight / 2 + swordleftheight,
-				wizardX, wizardY, wizardX + wizardWidth, wizardY + wizardHeight)) {
-					wizardHealth -= 1;
-				}
-			}
-  	} else if (code == 75) { // down
-			if (player == "kid") {
-  			if (boomerangX == -100) {
-  				boomerangX = xPos;
-  				boomerangY = yPos;
-  				boomerangStartX = xPos;
-  				boomerangStartY = yPos;
-  				boomerangSpeedX = 0;
-  				boomerangSpeedY = 10;
-  			}
-  		} else if (player == "warrior") {
-				canvasContext.drawImage(sworddown, xPos + warriorWidth / 2, yPos + warriorHeight / 2);
-				if (overlap(xPos + warriorWidth / 2, yPos + warriorHeight / 2, xPos + warriorWidth / 2 + swordleftwidth, yPos + warriorHeight / 2 + swordleftheight,
-				wizardX, wizardY, wizardX + wizardWidth, wizardY + wizardHeight)) {
-					wizardHealth -= 1;
-				}
-			}
-  	} else if (code == 65) { // a
-			xPos -= playerSpeed;
-  	} else if (code == 87) { // w
-			yPos -= playerSpeed;
-  	} else if (code == 68) { // d
-			xPos += playerSpeed;
-  	} else if (code == 83) { // s
-			yPos += playerSpeed;
-  	} else if (code == 49 && kidHealth > 0) {
-  		player = "kid";
-  		speed = 10;
-  	} else if (code == 50 && warriorHealth > 0) {
-  		player = "warrior";
-  		speed = 12;
-  	} else if (code == 51 && wizardHealth > 0) {
-  		player = "wizard";
-  		speed = 7;
-  	}
-  }
-}
-
 
 function mousedown(evt) {
   var rect = canvas.getBoundingClientRect();
@@ -184,35 +79,115 @@ function mousedown(evt) {
 
 	clickX = evt.clientX - rect.left - root.scrollLeft;
 	clickY = evt.clientY - rect.top - root.scrollTop;
+
+  if (state == 1) {
+    state = 2;
+  }
 }
 
-function updateAll() { // call this with every frame
-	// move and draw everything over the current canvas
-	keepLegal();
+var up = false;
+var down = false;
+var left = false;
+var right = false;
+
+document.onkeyup = keyRelease;
+
+function keyRelease(event) {
+	var code = event.keyCode;
+	if (code == 65) {
+		left = false;
+	} else if (code == 87) {
+		up = false;
+	} else if (code == 68) {
+		right = false;
+	} else if (code == 83) {
+		down = false;
+  }
+}
+
+function keyInput(event) {
+  wizard = new Image();
+  wizard.src = 'images/wizard.png';
+
+  wizardWidth = wizard.naturalWidth;
+  wizardHeight = wizard.naturalHeight;
+
+  sworddown = new Image();
+  sworddown.src = 'images/sworddown.png';
+
+  swordleft = new Image();
+  swordleft.src = 'images/swordleft.png';
+
+  swordup = new Image();
+  swordup.src = 'images/swordup.png';
+
+  swordright = new Image();
+  swordright.src = 'images/swordright.png';
+
+  swordleftwidth = swordleft.naturalWidth;
+  swordleftheight = swordleft.naturalHeight;
+
+  swordupwidth = swordup.naturalWidth;
+  swordupheight = swordup.naturalHeight;
+
+  var code = event.keyCode;
+
+  if (state == 2) {
+    if (code == 65) {
+      left = true;
+    } else if (code == 87) {
+      up = true;
+    } else if (code == 68) {
+      right = true;
+    } else if (code == 83) {
+      down = true;
+    } else if (code == 49 && kidHealth > 0) {
+      player = "kid";
+      playerSpeed = 5;
+      playerWidth = kid.naturalWidth;
+      playerHeight = kid.naturalHeight;
+    } else if (code == 50 && warriorHealth > 0) {
+      player = "warrior";
+      playerSpeed = 7;
+      playerWidth = warrior.naturalWidth;
+      playerHeight = warrior.naturalHeight;
+    } else if (code == 51) {
+      player = "wizard";
+      playerSpeed = 3;
+      playerWidth = wizard.naturalWidth;
+    }
+  }
+}
+
+function updateAll() {
+  keepLegal();
   drawAll();
-	moveAll();
-  console.log(state);
+  moveAll();
 }
 
 function keepLegal() { // ensure that the current position of the player is still legal
 	if (kidHealth <= 0 && warriorHealth <= 0 && wizardHealth <= 0) {
 		kidHealth = 0;
+    warriorHealth = 0;
+    wizardHealth = 0;
 		var r = confirm("Game over. Play again?");
 		if (r == true) {
-			window.open("stage4.html", "_self")
+			window.open("stage2.html", "_self")
 		}
-	} else if (wizardHealth <= 0) { // the new enemy's health is less than or equal to 0
+	} else if (wizardHealth <= 0) { //REPLACE WITH THE REAL ENEMY LATER
 		warriorHealth = 0;
 		gameover = true;
 		wizardX = -100;
 		wizardY = -100;
 		fireX = -100;
 		fireY = -100;
-	} else if (kidHealth <= 0) {
+	} else if (player == "kid" && kidHealth <= 0) {
 		player = "warrior";
-	} else if (warriorHealth <= 0) {
-		player = "kid";
-	}
+	} else if (player == "warrior" && warriorHealth <= 0) {
+		player = "wizard";
+	} else if (player == "wizard" && wizardHealth <= 0) {
+    player = "kid";
+  }
 
 	if (gameover) {
 		if (xPos > canvas.width) {
@@ -272,15 +247,88 @@ function keepLegal() { // ensure that the current position of the player is stil
 }
 
 function moveAll() {
-  if (state == 1) {
-    if (xPos < 400) {
+  //player movement
+  if (state == 0) {
+    if (xPos <= 500) {
       xPos += 3;
     } else {
-      state = 2;
+      state = 1;
     }
-  } else if (state == 1.1) {
-
+  } else if (state == 1) {
+    canvasContext.font = "20px Arial";
+    colorRect(400, 200, 700, 100, 'white');
+    colorText("Builder: Please! Save me from these monsters!", 470, 250, 'black');
+  } else if (state == 2) {
+    if (left) { // left
+      xPos -= playerSpeed;
+    } else if (up) { // up
+      yPos-= playerSpeed;
+    } else if (right) { // right
+      xPos += playerSpeed;
+    } else if (down) { // down
+      yPos += playerSpeed;
+    }
   }
+}
+
+function drawAll() {
+  drawBackGround('black', 'white');
+
+  fire = new Image();
+	fire.src = 'images/fire.png';
+
+	kid = new Image();
+	kid.src = 'images/kid.png';
+
+	warrior = new Image();
+	warrior.src = 'images/warrior.png'
+
+	wizard = new Image();
+	wizard.src = 'images/wizard.png';
+
+	boomerang = new Image();
+	boomerang.src = 'images/boomerang.png';
+
+  builder = new Image();
+  builder.src = 'images/builder.png';
+
+  builderWidth = builder.naturalWidth;
+  builderHeight = builder.naturalHeight;
+
+	fireWidth = fire.naturalWidth;
+	fireHeight = fire.naturalHeight;
+
+  kidWidth = kid.naturalWidth;
+  kidHeight = kid.naturalHeight;
+
+  warriorWidth = warrior.naturalWidth;
+  warriorHeight = warrior.naturalHeight;
+
+	wizardWidth = wizard.naturalWidth;
+	wizardHeight = wizard.naturalHeight;
+
+  canvasContext.drawImage(builder, builderX, builderY);
+
+  if (player == "kid") {
+    canvasContext.drawImage(kid, xPos, yPos);
+  } else if (player == "warrior") {
+    canvasContext.drawImage(warrior, xPos, yPos);
+  } else if (player == "wizard") {
+    canvasContext.drawImage(wizard, xPos, yPos);
+  }
+
+  canvasContext.drawImage(kid, 0, edge);
+  canvasContext.font = "10px sans-serif";
+  colorText(kidHealth, 0, edge + kidHeight + 10, 'black');
+  canvasContext.drawImage(warrior, 0, edge + kidHeight + 10 + 10);
+  canvasContext.font = "10px sans-serif";
+  colorText(warriorHealth, 0, edge + kidHeight + 10 + 10 + warriorHeight + 10, 'black');
+  canvasContext.drawImage(wizard, 0, edge + kidHeight + 10 + 10 + warriorHeight + 20);
+  colorText(wizardHealth, 0, edge + kidHeight + 10 + 10 + warriorHeight + 10 + wizardHeight + 20, 'black');
+
+  canvasContext.drawImage(builder, 0, edge * 10);
+  canvasContext.font = "10px sans-serif";
+  colorText(builderHealth, 0, edge * 10 + builderHeight + 10, 'black');
 }
 
 function drawBackGround(color1, color2) {
@@ -294,41 +342,23 @@ function drawBackGround(color1, color2) {
 	colorRect(canvas.width - edge, 0, edge, canvas.height, color2);
 }
 
-function drawAll() { // update the location of everything in the canvas
-  drawBackGround('black', 'white');
-
-	kid = new Image();
-	kid.src = 'images/kid.png';
-
-  var kidWidth = kid.naturalWidth;
-  var kidHeight = kid.naturalHeight;
-
-  warrior = new Image();
-  warrior.src = 'images/warrior.png';
-
-  var warriorWidth = warrior.naturalWidth;
-  var warriorHeight = warrior.naturalHeight;
-
-  wizard = new Image();
-  wizard.src = 'images/wizard.png';
-
-  var wizardWidth = wizard.naturalWidth;
-  var wizardHeight = wizard.naturalHeight;
-
-  playerWidth = kid.naturalWidth;
-  playerHeight = kid.naturalHeight;
-
-  canvasContext.drawImage(kid, 0, edge);
-  canvasContext.font = "10px sans-serif";
-  colorText(kidHealth, 0, edge + kidHeight + 10, 'black');
-  canvasContext.drawImage(warrior, 0, edge + kidHeight + 10 + 10);
-  canvasContext.font = "10px sans-serif";
-  colorText(warriorHealth, 0, edge + kidHeight + 10 + 10 + warriorHeight + 10, 'black');
-  canvasContext.drawImage(wizard, 0, edge + kidHeight + 10 + 10 + warriorHeight + 10 + 10);
-  canvasContext.font = "10px sans-serif";
-  colorText(wizardHealth, 0, edge + kidHeight + 10 + 10 + warriorHeight + 10 + 10 + wizardHeight + 10, 'black');
-
-  canvasContext.drawImage(kid, xPos, yPos);
+function overlap(x1, y1, X1, Y1, x2, y2, X2, Y2) {
+	if (x1 > x2 && y1 > y2) {
+		return overlap(x2, y2, X2, Y2, x1, y1, X1, Y1);
+	}
+	if (x1 <= x2 && x2 <= X1 && y1 <= y2 && y2 <= Y1) {
+		return true;
+	}
+	if (x1 <= x2 && x2 <= X1 && y1 <= Y2 && Y2 <= Y1) {
+		return true;
+	}
+	if (x1 <= X2 && X2 <= X1 && y1 <= Y2 && Y2 <= Y1) {
+		return true;
+	}
+	if (x1 <= X2 && X2 <= X1 && y1 <= y2 && y2 <= Y1) {
+		return true;
+	}
+	return false;
 }
 
 // helper function to draw a square
@@ -345,6 +375,7 @@ function colorRect(topLeftX,topLeftY, boxWidth,boxHeight, fillColor) {
 	canvasContext.fillRect(topLeftX,topLeftY, boxWidth,boxHeight);
 }
 
+//10px sans-serif
 // helper function to draw text
 function colorText(showWords, textX,textY, fillColor) {
 	canvasContext.fillStyle = fillColor;
